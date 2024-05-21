5860ac0eb1bbf81f1ebd7f4b516f688e17618293
RomanAfzal <rumanafzal97@gmail.com>
2024-05-21 18:52:02 +0500

    Initial Commit

---
+++ IOSWeatherApp/IOSWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSWeatherAppApp.swift
+//  IOSWeatherApp
+//
+//  Created by RomanAfzal on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct IOSWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
